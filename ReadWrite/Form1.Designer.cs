b3d863251e4c56f48b5f59c3104c25c4770a7d3d
JadonF <36163291+JadonF@users.noreply.github.com>
2018-05-25 10:37:07 -0400

    Initial commit

---
+++ ReadWrite/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ReadWrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(12, 37);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(247, 13);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Compare strings on a text file. (Case doesn\'t matter)";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(95, 73);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "Form1";
+            this.Text = "Read/Write File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ ReadWrite/Form1.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Created by: Jadon F
+ * Created on: 25-May-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #37 - Read/Write File
+ * This program reads a file to check if the strings on each line are equal.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ReadWrite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool StringsAreEqual(string word1, string word2)
+        {
+            //local variables
+            string upperW1;
+            string upperW2;
+            string arrayW1;
+            string arrayW2;
+            bool isEqual = false;
+
+            //define variables
+            arrayW1 = word1;
+            arrayW2 = word2;
+            upperW1 = arrayW1.ToUpper();
+            upperW2 = arrayW2.ToUpper();
+
+            //check
+            if (word1.Length == word2.Length)
+            {
+                if (upperW1 == upperW2)
+                {
+                    isEqual = true;
+                }
+            }
+            return isEqual;
+        }
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            //create arrays and variables
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            char[] charSeparators = new char[] { ' ', '\t' };
+
+            string output = "";
+
+            //foreach loop
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (StringsAreEqual(words[0], words[1]) == true)
+                {
+                    output = output + "True\r\n";
+                }
+                else
+                {
+                    output = output + "False\r\n";
+                }
+            }
+            //write the output file
+            System.IO.File.WriteAllText(@"output.txt", output);
+
+            //notify user it has been written
+            MessageBox.Show("The output file has been written.", "Output");
+        }
+    }
+}
